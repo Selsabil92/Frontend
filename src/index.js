dc183036364f1633a208de3c7e4968830497248a
Selsabil 👩‍💻 <94525950+Selsabil92@users.noreply.github.com>
2025-03-22 19:04:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// Si tu utilises un provider pour le contexte ou des fonctionnalités supplémentaires, tu peux les ajouter ici
-
 ReactDOM.render(
   <Router>
     <App />
